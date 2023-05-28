6c76c615b0da942cd2cfd9d72e65307d637732d1
Fardin2112 <fardinalam2112@gmail.com>
2023-05-29 00:38:44 +0530

    update

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
-let shhopinglist =[]
+let shoppinglist =[]
 const inputEL = document.getElementById("input-el")
 const addbtn = document.getElementById("add-el")
+const ulEL = document.getElementById("ul-el")
 
 addbtn.addEventListener("click",function(){
-    shhopinglist.push(inputEL.value)
-    
+    shoppinglist.push(inputEL.value) 
+    render(shoppinglist)  
+    inputEL.value=""
 })
+
+function render(shoppinglist){
+    let listitem = ""
+    for(let i = 0; i<shoppinglist.length; i++){
+        listitem += `
+            <li>
+                ${shoppinglist[i]}
+            </li>
+        `
+    }
+    ulEL.innerHTML = listitem
+}
