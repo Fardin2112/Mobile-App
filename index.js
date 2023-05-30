f2bd7e9247af8530e71753ec6ab3d639b70bf936
Fardin2112 <fardinalam2112@gmail.com>
2023-05-30 23:34:10 +0530

    update

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
-let shoppinglist =[]
-const inputEL = document.getElementById("input-el")
+// firbase connecting
+import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import{getDatabase,ref,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+const appSettings = {
+    databaseURL: "https://shopping-list-2666f-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB =ref(database,"ShoopingList")
+
+// App
+let array = []
+const inputEl = document.getElementById("input-el")
 const addbtn = document.getElementById("add-el")
 const ulEL = document.getElementById("ul-el")
 
 addbtn.addEventListener("click",function(){
-    shoppinglist.push(inputEL.value) 
-    render(shoppinglist)  
-    inputEL.value=""
+    let shoppinglist = inputEl.value
+    push(shoppingListInDB,shoppinglist) 
+    array.push(shoppinglist)
+    render(array)  
+    inputEl.value=""
 })
 
-function render(shoppinglist){
+function render(list){
     let listitem = ""
-    for(let i = 0; i<shoppinglist.length; i++){
+    for(let i = 0; i<list.length; i++){
         listitem += `
             <li>
-                ${shoppinglist[i]}
+                ${list[i]}
             </li>
         `
     }
