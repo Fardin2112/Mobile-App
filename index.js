ca665977bd8f94fe5321e7d225d248dd016df001
Fardin2112 <fardinalam2112@gmail.com>
2023-05-31 23:49:10 +0530

    update

--- index.js
+++ index.js
@@ -1,35 +1,44 @@
-// firbase connecting
-import {initializeApp} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import{getDatabase,ref,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
 const appSettings = {
     databaseURL: "https://shopping-list-2666f-default-rtdb.asia-southeast1.firebasedatabase.app/"
 }
+
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppingListInDB =ref(database,"ShoopingList")
+const shoppingListInDB = ref(database, "shoppingList")
 
-// App
-let array = []
-const inputEl = document.getElementById("input-el")
-const addbtn = document.getElementById("add-el")
-const ulEL = document.getElementById("ul-el")
+const inputFieldEl = document.getElementById("input-el")
+const addButtonEl = document.getElementById("add-el")
+const shoppingListEl = document.getElementById("ul-el")
 
-addbtn.addEventListener("click",function(){
-    let shoppinglist = inputEl.value
-    push(shoppingListInDB,shoppinglist) 
-    array.push(shoppinglist)
-    render(array)  
-    inputEl.value=""
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    clearInputFieldEl()
 })
 
-function render(list){
-    let listitem = ""
-    for(let i = 0; i<list.length; i++){
-        listitem += `
-            <li>
-                ${list[i]}
-            </li>
-        `
+onValue(shoppingListInDB, function(snapshot) {
+    let itemsArray = Object.values(snapshot.val())
+    
+    clearShoppingListEl()
+    
+    for (let i = 0; i < itemsArray.length; i++) {
+        appendItemToShoppingListEl(itemsArray[i])
     }
-    ulEL.innerHTML = listitem
+})
+
+function clearShoppingListEl() {
+    shoppingListEl.innerHTML = ""
+}
+
+function clearInputFieldEl() {
+    inputFieldEl.value = ""
+}
+
+function appendItemToShoppingListEl(itemValue) {
+    shoppingListEl.innerHTML += `<li>${itemValue}</li>`
 }
